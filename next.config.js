bdfd9efa7a66b96b4c6e0322ca80a8ed460c1076
Anderson Roque <andersonbarrinha18@gmail.com>
2023-02-07 16:18:18 -0300

    c

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     const ContentSecurityPolicy = `
   default-src 'none';
   script-src * data: https://ssl.gstatic.com 'unsafe-inline' 'unsafe-eval';
-  style-src '';
+  style-src https://fonts.googleapis.com 'unsafe-inline';
   font-src 'self' https://fonts.gstatic.com;  
   img-src 'self';
   base-uri 'self';
